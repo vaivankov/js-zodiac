93fe34647588baf9d2cbf66bcec1c88d7d6189e7
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-10-19 08:45:35 +0300

    Add in Dom on/off methods

--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -1,10 +1,36 @@
 /**
+ *
+ * @param {String|HTMLElement} selector - Имя селектора или нода
+ * @return {Dom} Новый Dom instance
+ */
+export function $$(selector) {
+  return new Dom(selector);
+}
+
+/**
+ * @property {Function} create - Создаёт новый тег и
+ * оборачивает его в Dom instance
+ * @param {String} tagName - Название тега
+ * @param {String} className - Имя css-класса
+ * @return {Dom} Новый Dom instance
+ */
+$$.create = (tagName, className = '') => {
+  const el = document.createElement(tagName);
+
+  if (className) {
+    el.classList.add(className);
+  }
+
+  return $$(el);
+};
+
+/**
  * Служебный класс позволяющий удобно работать с nodes
  */
 class Dom {
   /**
    *
-   * @param {String|HTMLElement} selector Имя селектора или нода
+   * @param {String|HTMLElement} selector - Имя селектора или нода
    */
   constructor(selector) {
     this.$element = typeof selector === 'string' ?
@@ -13,16 +39,16 @@ class Dom {
   }
 
   /**
-   * @property {Function} getHTML Достаёт вёрстку из $element
-   * @return {String} Node
+   * @property {Function} getHTML - Достаёт вёрстку из $element
+   * @return {String} - Node
    */
   getHTML() {
     return this.$element.outerHTML.trim();
   }
 
   /**
-   * @property {Function} setHTML Меняет содержимое $element
-   * @param {*} html Контент
+   * @property {Function} setHTML - Меняет содержимое $element
+   * @param {*} html - Контент
    * @return {Object} this
    */
   setHTML(html) {
@@ -33,8 +59,8 @@ class Dom {
   }
 
   /**
-   * @property {Function} addClasses Добавляет css-классы к $element
-   * @param {Array} className Имя классов
+   * @property {Function} addClasses - Добавляет css-классы к $element
+   * @param {Array} className - Имя классов
    * @return {Object} this
    */
   addClasses(className) {
@@ -48,8 +74,8 @@ class Dom {
   }
 
   /**
-   * @property {Function} removeClasses Удаляет css-классы у $element
-   * @param {Array} className Имя классов
+   * @property {Function} removeClasses - Удаляет css-классы у $element
+   * @param {Array} className - Имя классов
    * @return {Object} this
    */
   removeClasses(className) {
@@ -61,30 +87,32 @@ class Dom {
 
     return this;
   }
-}
 
-/**
- *
- * @param {String|HTMLElement} selector Имя селектора или нода
- * @return {Dom} Новый Dom instance
- */
-export function $$(selector) {
-  return new Dom(selector);
-}
 
-/**
- * @property {Function} create Создаёт новый тег и
- * оборачивает его в Dom instance
- * @param {String} tagName Название тега
- * @param {String} className Имя css-класса
- * @return {Dom} Новый Dom instance
- */
-$$.create = (tagName, className = '') => {
-  const el = document.createElement(tagName);
-
-  if (className) {
-    el.classList.add(className);
+  /**
+   * @property {Function} on - Добавляет к $element событие
+   * @param {Object} eventType - Название события
+   * @param {Object} callback - Функция для выполнения при событии
+   * @return {void}
+   */
+  on(eventType, callback) {
+    this.$element.addEventListener(
+        eventType,
+        callback
+    );
   }
 
-  return $$(el);
-};
+  /**
+   * @property {Function} off - Удаляет у $element событие
+   * @param {Object} eventType - Название события
+   * @param {Object} callback - Функция для выполнения при событии
+   * @return {void}
+   */
+  off(eventType, callback) {
+    this.$element.removeEventListener(
+        eventType,
+        callback
+    );
+  }
+}
+
