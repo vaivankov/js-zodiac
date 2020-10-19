4bb282fd35fc3c39a0c17eabb2232726c6d09e50
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-10-19 09:06:06 +0300

    Create getMethodName function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,22 @@
 /**
+ * @module utils/utils
  * @property {Function} capitalize - Достаёт вёрстку из $element
  * @param {String} string - Строка для преобразования
- * @return {String} Node
+ * @return {String} Строка с заглавной буквой
  */
 export function capitalize(string) {
   if (typeof string !== 'string') {
     return '';
   }
   return string.charAt(0).toLocaleUpperCase() + string.slice(1);
+}
+
+/**
+ * @module utils/utils
+ * @property {Function} getMethodName - Возвращает новое имя метода
+ * @param {String} eventName - Название события
+ * @return {String} Новое имя метода
+ */
+export function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
