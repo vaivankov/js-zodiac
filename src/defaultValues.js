2c4c20ccf8d1115faf4a6676cbb6c360daa09c24
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-10-26 16:32:57 +0300

    Create manageStore functions

---
+++ src/defaultValues.js
@@ -0,0 +1,17 @@
+/**
+ * @module constants
+ * @type {Object} defaultChartState - стандартное состояние карты
+*/
+export const defaultChartState = {
+  atma: -1,
+  buddha: -1,
+  chandra: -1,
+  dara: -1,
+  guru: -1,
+  ketu: -1,
+  mangala: -1,
+  rahu: -1,
+  surya: -1,
+  zani: -1,
+  zukra: -1,
+};
